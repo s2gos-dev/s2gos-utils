a1c672a368ab919171cfb582ef15b61cd8e131ab
nicolae marton <nicolae.marton@gmail.com>
2026-01-08 14:04:35 +0100

    add check for `https` protocol when performing path resolution.

--- src/s2gos_utils/io/resolver.py
+++ src/s2gos_utils/io/resolver.py
@@ -89,6 +89,13 @@ class FileResolver:
         """
         upath = UPath(path)
 
+        # NOTE: The "https" protocol returns False on calls to `exists`. This is 
+        # a hack to go around the issue. We need to understand which protocols 
+        # are unreliable and either find a reliable check or bypass the list 
+        # of unreliable protocols.
+        if (upath.is_absolute() and upath.protocol == "https"):
+            return upath
+
         # If already absolute or remote and exists, return as-is
         if (upath.is_absolute() or upath.protocol != "file") and upath.exists():
             return upath
